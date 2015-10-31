15e48f7fd6ccede5d7d001721f5de1814b821049
memsharded <james@conan.io>
2015-10-31 10:56:55 +0100

    initial work, working in Win VS12

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conans import ConanFile
+
+
+class ZMQCPPConan(ConanFile):
+	""" ONGOING WORK, tested in Win VS 12
+	"""
+    name = "zmqcpp"
+    version = "4.1.1"
+    url = "https://github.com/memsharded/conan-zmqcpp.git"
+    requires = "ZMQ/4.1.1@memsharded/testing"
+    exports = "zmq.hpp"
+
+    def package(self):
+        self.copy_headers("*.hpp")
