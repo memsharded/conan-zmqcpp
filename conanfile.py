29a146a5fc704bdce7de820e043b37a37da6bd1c
Eric Pederson <ericacm+github@gmail.com>
2016-12-03 02:17:35 -0500

    Update to 4.2.0.  Rename to cppzmq

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,24 @@
 from conans import ConanFile
 
 
-class ZMQCPPConan(ConanFile):
-    name = "zmqcpp"
-    version = "4.1.1"
+class CPPZMQConan(ConanFile):
+    name = "cppzmq"
+    version = "4.2.0"
+    sha1 = "6c9103433f978e2c190c806a98c53f5cee4744f9"  # For 4.2.0
     url = "https://github.com/memsharded/conan-zmqcpp.git"
-    requires = "ZMQ/4.1.1@memsharded/stable"
-    exports = "zmq.hpp"
+    requires = "libzmq/4.2.0@memsharded/stable"
+    source_dir = "cppzmq"
+
+    def source(self):
+        self.run_command("git clone https://github.com/zeromq/cppzmq.git")
+        self.run_command("git checkout -b %s.x %s" % (self.version, self.sha1), self.source_dir)
+
+    def run_command(self, cmd, cwd=None):
+        self.output.info(cmd)
+        self.run(cmd, True, cwd)
 
     def package(self):
-        self.copy_headers("*.hpp")
+        self.copy("*.hpp", dst="include", src="%s" % self.source_dir)
+
+    def package_info(self):
+        self.cpp_info.libdirs = []
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class ZMQCppTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "zmqcpp/4.1.1@memsharded/stable"
+    requires = "zmqcpp/4.2.0@memsharded/stable"
     generators = "cmake"
 
     def build(self):
@@ -19,7 +19,7 @@ class ZMQCppTestConan(ConanFile):
         server = "bin%sserver" % os.sep
         import subprocess
         pid = subprocess.Popen(server)
-        print "Lets launch client for ", server
+        print("Lets launch client for ", server)
 
         os.chdir("bin")
         self.run(".%sclient > null" % os.sep)
